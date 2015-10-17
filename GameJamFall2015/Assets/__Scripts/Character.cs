cca2c7bcc5b043f593e1827d851fda7318326a09
Haizhou Zhao <zhaohaizhou940527@gmail.com>
2015-10-16 23:47:34 -0400

    Add player pickup key mechanism

--- GameJamFall2015/Assets/__Scripts/Character.cs
+++ GameJamFall2015/Assets/__Scripts/Character.cs
@@ -14,6 +14,8 @@ public class Character : MonoBehaviour {
 	bool hitWall;
 	int groundPhysicsLayerMask;
 
+	int keyCount = 0;
+
 	// Use this for initialization
 	void Start () {
 		rigid = GetComponent<Rigidbody> ();
@@ -53,8 +55,11 @@ public class Character : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 		GameObject collidedWith = other.gameObject;
 		
-		if (collidedWith.tag == "Ground"){
+		if (collidedWith.tag == "Ground") {
 			jumping = false;
+		} else if (collidedWith.tag == "Key") {
+			++keyCount;
+			Destroy (collidedWith);
 		}
 	}
 
