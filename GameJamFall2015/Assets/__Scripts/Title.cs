348737653bd8d2825502f6a48aaccf7d89a0f365
HwangN <nhwa@umich.edu>
2015-10-17 12:46:46 -0400

    Title

---
+++ GameJamFall2015/Assets/__Scripts/Title.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour {
+
+	Text start;
+
+	// Use this for initialization
+	void Start () {
+		start = GetComponentInChildren<Text> ();
+		StartCoroutine ("Wait");
+	}
+
+	// Update is called once per frame
+	void Update(){
+		if (Input.anyKeyDown) {
+			Application.LoadLevel ("_Scene_Main_NH");
+		}
+	}
+
+	IEnumerator Wait(){
+		start.enabled = false;
+		yield return new WaitForSeconds (1);
+		start.enabled = true;
+	}
+}
