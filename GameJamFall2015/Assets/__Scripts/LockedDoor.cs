51ad3dbb8f115b8ad925ba6d4939bcc0e025bb20
HwangN <nhwa@umich.edu>
2015-10-17 02:48:58 -0400

    GitHub please just let me merge stuff without issues.

---
+++ GameJamFall2015/Assets/__Scripts/LockedDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedDoor : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		GameObject collidedWith = other.gameObject;
+
+		if (collidedWith.tag == "Character") {
+			if (Character.S.keyCount != 0){
+				Character.S.keyCount--;
+				Destroy(gameObject);
+			}
+		}
+
+
+	}
+
+}
