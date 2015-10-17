8ae649f479931b40ceea5d156c1f8755006156d1
joshviss <joshviss@umich.edu>
2015-10-17 10:59:35 -0400

    Merge Conflict Resolve

---
+++ GameJamFall2015/Assets/Torch.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+	public float timeLitToDestoryed = 10f;
+	public bool characterCollided = false;
+	public float minFlickerSpeedOn = 0.05f;
+	public float maxFlickerSpeedOn = 0.1f;
+	public float minFlickerSpeedOff = 0.2f;
+	public float maxFlickerSpeedOff = 0.3f;
+	public float minLightIntensity = 6f;
+	public float maxLightIntensity = 8f;
+
+	Rigidbody rigid;
+	SpriteRenderer spRend;
+	Color torchColor;
+	Light pointLight;
+	public GameObject characterObject;
+
+	RigidbodyConstraints noRotZ, noRotYZ;
+	Quaternion turnLeft = Quaternion.Euler(0, 180, 0);
+	Quaternion rotate90 = Quaternion.Euler(0, 0, 90);
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+		spRend = transform.Find("Sprite").GetComponent<SpriteRenderer>();
+		pointLight = transform.Find("TorchPointLight").GetComponent<Light>();
+		characterObject = GameObject.Find("Character");
+
+		torchColor = spRend.color;
+
+		noRotZ = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+		noRotYZ = noRotZ | RigidbodyConstraints.FreezePositionY;
+		StartCoroutine(LightFlicker());
+		//StartCoroutine(LightColor());
+		//StartCoroutine(CheckForCharacter());
+	}
+
+	IEnumerator LightFlicker() {
+		while (true) {
+			pointLight.intensity = Random.Range(minLightIntensity, maxLightIntensity);
+			yield return new WaitForSeconds (Random.Range(minFlickerSpeedOff, maxFlickerSpeedOff));
+			pointLight.intensity = Random.Range(minLightIntensity, minLightIntensity);
+			yield return new WaitForSeconds(Random.Range(minFlickerSpeedOn, maxFlickerSpeedOn));
+		}
+	}
+
+	/*
+	IEnumerator LightColor() {
+		
+		while (true) {
+			pointLight.color = new Color();
+			yield return new WaitForSeconds(Random.Range(minFlickerSpeedOff, maxFlickerSpeedOff));
+			pointLight.intensity = Random.Range(minLightIntensity, minLightIntensity);
+			yield return new WaitForSeconds(Random.Range(minFlickerSpeedOn, maxFlickerSpeedOn));
+		}
+		
+	}
+	
+
+	IEnumerator CheckForCharacter() {
+		yield return null;
+	}
+	*/
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	// FixedUpdate is called once per physics engine update (50 fps)
+	void FixedUpdate () {
+	
+	}
+}
--- GameJamFall2015/Assets/__Scripts/Character.cs
+++ GameJamFall2015/Assets/__Scripts/Character.cs
@@ -1,127 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class Character : MonoBehaviour {
 
-	static public Character S; //Singleton
-	float speedX = 2.0f;
+	float speedX = 4.0f;
 	float speedJump = 8.0f;
-	float speedLadder = 1.0f;
-	Rigidbody rigid;
-	RigidbodyConstraints noRotZ, noRotYZ;
-	BoxCollider body;
-	SpriteRenderer spRend;
-	bool grounded;
-	int groundPhysicsLayerMask;
-	int ladderLayerMask;
-	int health = 30;
-	int damage = 1;
-	public int keyCount = 0;
-	public Canvas gameOver;
+	float speedLadder = 4.0f;
+
+	bool ________________________________;
 	
-	bool onLadder = false;
-	bool collideWithLadder = false;
-	Vector3 ladderStartPosition;
+	Rigidbody rigid;
+	bool jumping;
+	bool hitWall;
+	int groundPhysicsLayerMask;
+
+	int health = 20;
+
+	int keyCount = 0;
 
 	// Use this for initialization
 	void Start () {
-		S = this;
 		rigid = GetComponent<Rigidbody> ();
-		spRend = GetComponent<SpriteRenderer> ();
 		groundPhysicsLayerMask = LayerMask.GetMask ("Ground");
-		ladderLayerMask = LayerMask.GetMask ("Ladder");
-		body = GetComponent<BoxCollider> ();
-		gameOver.enabled = false;
-
-		noRotZ = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
-		noRotYZ = noRotZ | RigidbodyConstraints.FreezePositionY;
-
-		InvokeRepeating ("DecreaseHealth", 0f, 1f);
 	}
-
-	void DecreaseHealth(){
-		health -= damage;
-	}
-
-	void Update(){
-		if (health <= 0) {
-			gameOver.enabled = true;
-			if (Input.anyKeyDown){
-				Application.LoadLevel ("_Scene_Main_NH");
-			}
-		}
-	}
-
+	
 	// FixedUpdate is called once per physics update
 	void FixedUpdate () {
 		Vector3 vel = rigid.velocity;
 
-		Vector3 loc = transform.position;
-		Debug.DrawRay (loc, Vector3.down * 0.5f, Color.blue);
-		grounded = (Physics.Raycast (loc, Vector3.down, 0.5f, groundPhysicsLayerMask));
+		/*
+		Vector3 bodyLoc = transform.position;
+		Debug.DrawRay (bodyLoc, transform.right * 0.3f);
+		hitWall = (Physics.Raycast (bodyLoc, transform.right, 0.3f, groundPhysicsLayerMask)
+		           || Physics.Raycast (bodyLoc + Vector3.up * 0.3f, transform.right, 0.3f, groundPhysicsLayerMask)
+		           || Physics.Raycast (bodyLoc + Vector3.down * 0.3f, transform.right, 0.3f, groundPhysicsLayerMask));
+		*/
 
 		// Left and Right Movement
-		if (Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow)) {
+		if (Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow) && !hitWall) {
 			vel.x = -speedX;
-		} else if (Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow)) {
+		} else if (Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow) && !hitWall) {
 			vel.x = speedX;
 		} else {
 			vel.x = 0;
 		}
 
 		// Jumping
-		if (Input.GetKeyDown (KeyCode.Space) && grounded && !onLadder) {
+		if (Input.GetKeyDown (KeyCode.Space) && !jumping) {
 			vel.y = speedJump;
+			jumping = true;
 		}
 
-		// Grab onto ladder
-		if (collideWithLadder) {
-			if (Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.DownArrow)) {
-				onLadder = true;
-			}
-		}
-
-		// Movement while on ladder
-		if (onLadder) {
-			if (Input.GetKey (KeyCode.UpArrow) && !Input.GetKey (KeyCode.DownArrow)) {
-				vel.y = speedLadder;
-			} else if (Input.GetKey (KeyCode.DownArrow) && !Input.GetKey (KeyCode.UpArrow)) {
-				vel.y = -speedLadder;
-			} else {
-				vel.y = 0;
-			}
-		}
-
-		// Jumping down ladder
-		if (onLadder && Input.GetKeyDown (KeyCode.Space)) {
-			onLadder = false;
-			rigid.useGravity = true;
-		}
-
-		// Set the velocity
 		rigid.velocity = vel;
 	}
 
-	void OnTriggerEnter(Collider other){
+	void OnCollisionEnter(Collision other){
 		GameObject collidedWith = other.gameObject;
-
-		if (collidedWith.tag == "Ladder") {
-			collideWithLadder = true;
-			//rigid.useGravity = false;
-			//ladderStartPosition = transform.position;
-			//ladderStartPosition.x = other.transform.position.x;
+		
+		if (collidedWith.tag == "Ground") {
+			jumping = false;
+		} else if (collidedWith.tag == "Key") {
+			++keyCount;
+			Destroy (collidedWith);
 		}
 	}
 
-	void OnTriggerExit(Collider other){
-		GameObject collidedWith = other.gameObject;
-
-		if (collidedWith.tag == "Ladder") {
-			collideWithLadder = false;
-			rigid.useGravity = true;
-			onLadder = false;
-		}
-	}
 }
 
