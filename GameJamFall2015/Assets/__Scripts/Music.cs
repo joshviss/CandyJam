ab35c39f81590f3b952ef8637fcbe325a277a728
HwangN <nhwa@umich.edu>
2015-10-17 21:45:40 -0400

    Elemental data

---
+++ GameJamFall2015/Assets/__Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Music : MonoBehaviour {
+
+	AudioSource song;
+	float volume = 0f;
+
+	// Use this for initialization
+	void Start () {
+		song = GetComponent<AudioSource> ();
+		FadeIn ();
+		song.Play ();
+	}
+
+	void FadeIn(){
+		while (volume < 1f) {
+			volume += 0.1f * Time.deltaTime;
+			song.volume = volume;
+		}
+	}
+}
