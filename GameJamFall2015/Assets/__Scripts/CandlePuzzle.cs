c7e33bb425dbec0f0ec62862b095af2870bb810b
joshviss <joshviss@umich.edu>
2015-10-18 17:36:01 -0400

    Candle Changes

---
+++ GameJamFall2015/Assets/__Scripts/CandlePuzzle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandlePuzzle : MonoBehaviour {
+
+	public GameObject Key;
+	public GameObject Candle1, Candle2, Candle3, Candle4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
