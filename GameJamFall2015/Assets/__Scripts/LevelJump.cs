2e123167888f8e6fa4cf6970a49a64bd5fa97d43
joshviss <joshviss@umich.edu>
2015-10-18 23:28:11 -0400

    Added a bunch of torches and sticks to level 1

---
+++ GameJamFall2015/Assets/__Scripts/LevelJump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelJump : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha0)) {
+			Application.LoadLevel("_Scene_Main_JV");
+		} else if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			Application.LoadLevel("_Scene_Level0");
+		} else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			Application.LoadLevel("_Scene_Level1");
+		} else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			Application.LoadLevel("_Scene_Level2");
+		}
+	
+	}
+}
