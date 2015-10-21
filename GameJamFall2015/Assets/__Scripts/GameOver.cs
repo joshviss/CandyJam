61e269907ba2525ae4c3735eb1b37260dea766cc
HwangN <nhwa@umich.edu>
2015-10-21 00:47:26 -0400

    Changed the Game Over Sceen

--- GameJamFall2015/Assets/__Scripts/GameOver.cs
+++ GameJamFall2015/Assets/__Scripts/GameOver.cs
@@ -4,19 +4,8 @@ using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		StartCoroutine ("Wait");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.anyKeyDown) {
-			Application.LoadLevel ("_Scene_Title");
-		}
+	public void TitleReturn(){
+		Application.LoadLevel ("_Scene_Title");
 	}
 
-	IEnumerator Wait(){
-		yield return new WaitForSeconds (3);
-	}
 }
